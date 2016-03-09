8a634261aef781c9e08f405e1aba340c2389eb60
Jaka Azman <azman.jaka@gmail.com>
2016-03-09 09:51:23 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', function() {
 	//stran nalozena
-		
+	var izvediPrijavo = function(event){
+			var uporabnik =document.querySelector("#uporabnisko_ime").value;
+			document.getElementById("uporabnik").innerHTML =uporabnik;
+			document.getElementsByClassName("pokrivalo").item(0).style.display = "none";
+			
+			
+		}
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
@@ -11,6 +18,11 @@ window.addEventListener('load', function() {
 			var cas = parseInt(casovnik.innerHTML);
 	
 			//TODO: 
+			while (cas > 0){
+				cas = cas - 1;
+			}
+			
+			
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
 		}
